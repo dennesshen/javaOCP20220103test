d709ee798c752dd1afdf0ce7686171e34358490d
student <student@192.168.1.109>
2022-01-10 21:06:38 +0800

    20220107 巨匠

--- ocp/src/main/java/stydy/ocp/day3/StringDemo.java
+++ ocp/src/main/java/stydy/ocp/day3/StringDemo.java
@@ -6,5 +6,6 @@ public class StringDemo {
 		System.out.printf("長度:%d\n", words.length());
 		System.out.printf("on 的位置在 %d\n", words.indexOf("on"));
 		System.out.printf("off 的位置在 %d\n",words.indexOf("off"));
+		System.out.println(words.split(" ").length);
 	}
 }
