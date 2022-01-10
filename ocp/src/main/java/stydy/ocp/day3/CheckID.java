b3d87902e093731670c393398d559708c70dd0f1
student <student@192.168.1.109>
2022-01-10 21:30:45 +0800

    巨匠

---
+++ ocp/src/main/java/stydy/ocp/day3/CheckID.java
@@ -0,0 +1,11 @@
+package stydy.ocp.day3;
+
+import java.util.Scanner;
+
+public class CheckID {
+	public static void main(String[] args) {
+		System.out.println("請輸入");
+		String id = new Scanner(System.in).next();
+		
+	}
+}
